04c85c50585e9f86e98bdf7e5e59ddcaca274176
Kay <info@beckmann-md.de>
2023-09-09 18:33:04 +0200

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatCardModule } from "@angular/material/card";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +26,11 @@ import { MatCardModule } from "@angular/material/card";
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: "registerWhenStable:30000"
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'personal-bilance',
+    appId: '1:823068149343:web:823ff2081b4a9931abbdea',
+    storageBucket: 'personal-bilance.appspot.com',
+    apiKey: 'AIzaSyBghgHkCCyyVoL0Of7WWN7zJ80H9f33xr4',
+    authDomain: 'personal-bilance.firebaseapp.com',
+    messagingSenderId: '823068149343',
+  },
+};
