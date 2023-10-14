cd9b452e85dacd8c7aa72c02fc5c344e212a7c6a
Kay <info@beckmann-md.de>
2023-10-14 16:36:47 +0200

    clean up code

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,16 +4,8 @@ import { CommonModule } from "@angular/common";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { DashboardComponent } from "./dashboard.component";
 
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
-
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    MatToolbarModule,
-    MatIconModule
-  ]
+  imports: [CommonModule, DashboardRoutingModule]
 })
 export class DashboardModule {}
